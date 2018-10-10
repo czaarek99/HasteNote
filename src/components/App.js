cf4ffa8df85499615f87ddda1eb6a7fb7d778e3c
Czarek <sarek123456789@gmail.com>
2018-10-10 18:06:51 +0200

    Prepare project for development

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/app.css';
+
+class App extends Component {
+  render() {
+    return (
+        <h4>Hi</h4>
+    );
+  }
+}
+
+export default App;
