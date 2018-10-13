a2f66bdca3dcd861440dcedb244089c5bbd97dbe
Czarek <sarek123456789@gmail.com>
2018-10-13 09:49:50 +0200

    Add session management

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const database = require("./database");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const port = process.env.PORT || 5000;
 
 async function startServer() {
@@ -8,11 +10,30 @@ async function startServer() {
         force: false
     });
     
-    app.listen(port);
+    app.use(cookieParser());
+    
+    const SequelizeStore =
+        require("connect-session-sequelize")(session.Store);
+    
+    const store = new SequelizeStore({
+        db: database.sequelize
+    });
+    
+    app.use(session({
+        saveUninitialized: false,
+        resave: false,
+        secret: "yAmoedgPiiNCxVvSfJ3j",
+        store,
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        }
+    }));
     
     app.get("/test", (req, res) => {
         res.send("Working fine");
     });
+    
+    app.listen(port);
 }
 
 startServer();
