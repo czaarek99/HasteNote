69e8ee7b25d8cae205d13a0206406ae96585b5d3
Czarek <sarek123456789@gmail.com>
2018-10-14 15:19:07 +0200

    Fix react warnings in LoadingGrid

--- src/components/LoadingGrid.jsx
+++ src/components/LoadingGrid.jsx
@@ -5,11 +5,11 @@ class LoadingGrid extends Component {
     render() {
         const cubes = [];
         for(let i = 1; i < 10; i++) {
-            cubes.push(<div className={"loadingCube loadingCube" + i}/>)
+            cubes.push(<div className={"loadingCube loadingCube" + i} key={i}/>)
         }
         
         return (
-            <div class="loadingCubeGrid">
+            <div className="loadingCubeGrid">
                 {cubes}
             </div>
         );
